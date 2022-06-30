5f5d0e62503c3d48629ff001ff4a9e3033fd4f6e
Arvid Ståhl Strömberg <arvostahl@gmail.com>
2022-06-30 15:03:45 +0200

    insert into todos

--- componets/TodoPage.tsx
+++ componets/TodoPage.tsx
@@ -1,6 +1,60 @@
-import { Text } from "@rneui/themed";
+import { View, TextInput } from "react-native";
+import React, { useEffect, useState } from "react";
+import { GetDb } from "../stores/Misc";
 
+const db = GetDb();
 
-export function TodoPage(){
-    return (<Text h1>Todos</Text>)
+export function TodoPage() {
+  const [todos, setItems] = useState(null);
+  const [text, setText] = useState(null);
+
+  useEffect(() => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        `select * from Todos where done = ?;`,
+        [0],
+        (_, { rows }) => {
+          setItems(rows._array);
+        }
+      );
+    });
+  }, []);
+
+  console.log(`todos: ${todos.length}`);
+
+  const add = (text) => {
+    // is text empty?
+    if (text === null || text === "") {
+      return false;
+    }
+
+    db.transaction((tx) => {
+      tx.executeSql(
+        "insert into Todos (done, value) values (0, ?)",
+        [text],
+        (_, { rows }) => {
+          console.log(`inserted row into Todos`);
+          setItems([...todos, rows._array[0]]);
+        },
+        (_, error) => {
+          console.log(error);
+          return true;
+        }
+      );
+    });
+  };
+
+  return (
+    <View>
+      <TextInput
+        onChangeText={(text) => setText(text)}
+        onSubmitEditing={() => {
+          add(text);
+          setText(null);
+        }}
+        placeholder="what do you need to do?"
+        value={text}
+      />
+    </View>
+  );
 }
