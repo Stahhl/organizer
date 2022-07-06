1c75e04ca163cd072f3b59af4645aefc70e5dd9d
Arvid Ståhl Strömberg <arvostahl@gmail.com>
2022-07-06 11:55:44 +0200

    refactored todoform to seperate component

---
+++ componets/TodoForm.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { Button, Modal, TextInput, View } from "react-native";
+import { addTodo, useAppDispatch } from "../state/Store";
+import tw from "twrnc";
+
+export type TodoFormProps = {
+  modalOpen: boolean;
+  modalToggle: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export function TodoForm(props: TodoFormProps) {
+  const [text, setText] = useState<string>(null);
+  const dispatch = useAppDispatch();
+
+  function SubmitTodoForm() {
+    dispatch(addTodo(text));
+    setText(null);
+    props.modalToggle(false);
+  }
+
+  return (
+    <Modal visible={props.modalOpen}>
+      <View style={tw`flex-1 flex-col m-1`}>
+        <TextInput
+          style={tw`flex-1 shadow-sm  text-center mt-auto`}
+          placeholder="what do you need to do?"
+          value={text}
+          onChangeText={setText}
+          onSubmitEditing={SubmitTodoForm}
+        />
+        <View style={tw`flex flex-row mt-auto`}>
+          <View style={tw`w-1/2`}>
+            <Button
+              title="Cancel"
+              color="red"
+              onPress={() => {
+                console.log("button - addTodo - cancel");
+                props.modalToggle(false);
+              }}
+            />
+          </View>
+          <View style={tw`w-1/2`}>
+            <Button
+              title="Save"
+              color="green"
+              onPress={() => {
+                console.log("button - addTodo - save");
+                SubmitTodoForm();
+              }}
+            />
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+}
--- pages/TodoPage.tsx
+++ pages/TodoPage.tsx
@@ -2,9 +2,8 @@ import { View, TextInput, FlatList, Modal, Text, Button } from "react-native";
 import React, { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector, addTodo, Todo } from "../state/Store";
 import { TodoList } from "../componets/TodoList";
+import { TodoForm } from "../componets/TodoForm";
 import tw from "twrnc";
-
-// const db = GetDb();
 
 export function TodoPage() {
   console.log("TodoPage");
@@ -23,40 +22,7 @@ export function TodoPage() {
 
   return (
     <View style={tw`flex-1`}>
-
-      <Modal visible={modalOpen} presentationStyle={"pageSheet"}>
-        <View style={tw`flex-1 flex-col m-1`}>
-          <TextInput
-            style={tw`flex-1 shadow-sm  text-center mt-auto`}
-            placeholder="what do you need to do?"
-            value={text}
-            onChangeText={setText}
-            onSubmitEditing={SubmitTodoForm}
-          />
-          <View style={tw`flex flex-row mt-auto`}>
-            <View style={tw`w-1/2`}>
-              <Button
-                title="Cancel"
-                color="red"
-                onPress={() => {
-                  console.log("button - addTodo - cancel");
-                  modalToggle(false);
-                }}
-              />
-            </View>
-            <View style={tw`w-1/2`}>
-              <Button
-                title="Save"
-                color="green"
-                onPress={() => {
-                  console.log("button - addTodo - save");
-                  SubmitTodoForm();
-                }}
-              />
-            </View>
-          </View>
-        </View>
-      </Modal>
+      <TodoForm {...{modalOpen, modalToggle}} />
 
       <FlatList
         data={todos}
